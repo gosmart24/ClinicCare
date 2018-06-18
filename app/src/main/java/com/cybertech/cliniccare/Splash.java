adf54325c7bdcad3b6f30e1e966109c80286c9fe
CyberTech <stagent24@gmail.com>
2018-06-18 19:49:14 +0100

    initial commit for this project

--- app/src/main/java/com/cybertech/cliniccare/Splash.java
+++ app/src/main/java/com/cybertech/cliniccare/Splash.java
@@ -23,7 +23,7 @@ public class Splash extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-//
+// hello
                     Intent intent = new Intent(Splash.this, Login.class);
                     startActivity(intent);
                     finish();
